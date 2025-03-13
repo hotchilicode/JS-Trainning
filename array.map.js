cb01c97a8855956bd498c0236ade482e516dbb04
chiliwilys <siticay28@kurbieh.com>
2025-03-13 16:03:03 -0600

    mapFixed

--- array.map.js
+++ array.map.js
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 
-    <style>
-
-        body {
-            background: rgb(0, 0, 0);
-            color: rgb(255, 248, 248);
-        }
-    </style>
-</head>
-<body>
-
-    <script>
-        
         // array.map() = executes a provided callback function
         //                   once for each element in an array
         //                   AND creates a new array
@@ -40,7 +22,3 @@
         }
 
 
-    </script>
-    
-</body>
-</html>
